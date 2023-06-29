4de978ff0bc92419541fa27e6905f9590f6693a8
diego <sandoval.vargas.diego.1c@gmail.com>
2023-06-29 11:33:54 -0600

    Refactor to Java Config Ctx

--- sfg-di/src/main/java/guru/springframework/sfgdi/services/SetterInjectedService.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/services/SetterInjectedService.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
+
 public class SetterInjectedService  implements GrettingService{
      @Override
     public String sayGretting() {
